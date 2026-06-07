cc9ff9be2d5e31fe570951405e40669a87b88548
dkkoma <dkkoma@gmail.com>
2026-06-07 10:09:10 +0900

    grpc-timeout header appendをprechecked化

--- src/transport.c
+++ src/transport.c
@@ -2241,7 +2241,13 @@ void append_grpc_timeout_request_header(h2_request_headers *headers, zend_long t
         zend_string_release(value_str);
         return;
     }
-    append_request_header(headers, "grpc-timeout", sizeof("grpc-timeout") - 1, ZSTR_VAL(value_str), ZSTR_LEN(value_str));
+    if (headers->len >= headers->capacity) {
+        grow_request_headers(headers);
+        if (headers->len >= headers->capacity) {
+            return;
+        }
+    }
+    append_request_header_unchecked(headers, "grpc-timeout", sizeof("grpc-timeout") - 1, ZSTR_VAL(value_str), ZSTR_LEN(value_str));
 }
 
 void append_user_agent_request_header(h2_request_headers *headers, zend_string *primary_user_agent)
